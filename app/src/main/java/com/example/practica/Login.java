41c38c8edf6076dd188008040dde3c72ade95a62
Iker Iturralde <ikerite02@gmail.com>
2023-01-16 13:47:34 +0100

    initial commit

---
+++ app/src/main/java/com/example/practica/Login.java
@@ -0,0 +1,63 @@
+package com.example.practica;
+
+        import androidx.appcompat.app.AppCompatActivity;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.view.animation.Animation;
+        import android.view.animation.AnimationUtils;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+
+        //importamos glide para el logo
+        //hay que añadir dos cosas en gradle Module (mirar ahi)
+        import com.bumptech.glide.Glide;
+        import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //LINEA PARA NO ACTION BAR
+        getSupportActionBar().hide();
+        //ANIMATION LOGO
+        TextView signup = findViewById(R.id.textsignup);
+
+        //metodo para que al pulsar el texto indicado en el activity login haga la animacion
+        signup.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              openSignup();
+          }
+          });
+
+        //GLIDE LOGO
+        ImageView mlogo = findViewById(R.id.logo);
+        Glide.with(this)
+                .load(R.drawable.persona)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .circleCrop()
+                .into(mlogo);
+
+        //ANIMATION LOGO
+
+        ImageView logo = findViewById(R.id.logo);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        logo.startAnimation(myanim);
+
+    }
+
+    //metodo para cambiar de pagina
+    public void openSignup() {
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
+    }
+    public void abrirlogin(View view) {
+        Intent intent = new Intent(Login.this, Signup.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/practica/Splash.java
@@ -0,0 +1,63 @@
+package com.example.practica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        getSupportActionBar().hide();
+        openApp();
+
+        //implements and starts animation
+        // objeto thunder sobre el cual aplicaremos la animación
+        ImageView thunder = findViewById(R.id.logosplash);
+
+        //creamos un objeto animación que incorpora la animación descrita en el xml y con el método
+        // startAnimation lo aplicamos al imageview del logo
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.blink);
+        thunder.startAnimation(myanim);
+
+        //    Glide para cargar el fondo
+        ImageView mSea = findViewById(R.id.fondo);
+
+        Glide.with(this)
+              .load(R.drawable.img)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+//                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.teal_200)))
+//                .circleCrop()
+                .into(mSea);
+
+    }
+
+//metodo para cambiar de pagina Splash - Login
+    private void openApp() {
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash
+                        .this, Login.class);
+                startActivity(intent);
+            }
+        }, 2000); //tiempo que tarda en pasar de pagina
+    }
+
+}
